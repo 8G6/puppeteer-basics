ad7849ddfb87d4711f5eb929c0ca86648f0906c4
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-08-27 23:25:15 +0530

    update

--- puppeteer-core/URL_to_PDF_Converter.js
+++ puppeteer-core/URL_to_PDF_Converter.js
@@ -1,14 +1,26 @@
 const puppeteer = require('puppeteer-core');
+const readline = require('readline');
+//For taking user input from terminal
+
+require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let NAME;
+readline.question(`Enter website URL : `, URL => {
+    NAME = URL
+    readline.close()
+})
 
 (async ()=>{
     const Browser = await puppeteer.launch({
         executablePath:"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" 
     })
-
+    
     const tab=await Browser.newPage()
     //wait for the new tab to to open
-
-    await tab.goto('http://google.com',{waitUntil:'networkidle2'})
+    
+    await tab.goto(URL,{waitUntil:'networkidle2'})
     .then(()=>{console.log("Gone to url and waited to fully load")})
     /*wait untill the site is fully loaded 
     networkidle2 argument considers navigation successful when 
