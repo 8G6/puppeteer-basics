fcb114c6b04aeeb2dd5a9859dff9a4bee6c562f1
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-08-27 22:32:21 +0530

    init

---
+++ puppeteer-core/Hello_World.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer-core');/*<- this semicolan is important don't 
+                                           forget to add it in this method */
+
+(async ()=>{
+    const Browser = await puppeteer.launch({
+        headless:false,
+        /* headless mode is defaultly true you
+        have to make it false to show browser GUI*/
+        executablePath:"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" 
+        /*For puppeteer-core 
+         puppeteer-core  don't have a buit in browser 
+         so we need to specify the path to browser binary*/
+    })
+    const tab=await Browser.newPage()
+    //wait for the new tab to to open
+    await tab.goto('http://google.com')
+    /*wait untill the site is loaded (this dont check
+    if the site is fully loaded or not)*/ 
+    await Browser.close()
+    //close the Browser after the site loaded
+})()
---
+++ puppeteer/Hello_World.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');/*<- this semicolan is important don't 
+                                        forget to add it in this method */
+(async ()=>{
+    const Browser = await puppeteer.launch({
+        headless:false,
+        /* headless mode is defaultly true you
+        have to make it false to show browser GUI*/
+    })
+    const tab=await Browser.newPage()
+    //wait for the new tab to to open
+    await tab.goto('http://google.com')
+    /*wait untill the site is loaded (this dont check
+    if the site is fully loaded or not)*/ 
+    await Browser.close()
+    //close the Browser after the site loaded
+})()
